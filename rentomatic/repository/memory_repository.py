b4f6e1327d29c7e34e4e7da811e48fb370c4da5c
shigaresso <70837100+shigaresso@users.noreply.github.com>
2024-08-04 19:26:26 +0900

    feat: #7 リポジトリクラスを実装し、データを永続化層から具体的に取り出す方法を定義しテストコードで動作の保証をした。
    アプリケーションで利用するデータは先ほどユースケースで返すように実装したように思えるが、なぜもう一度実装してるのだろうか。
    先ほど実装したユースケースの話をするとリポジトリが取得してきたデータを呼び出し元に渡すだけだ。
    これによってリポジトリクラスを変更することでデータをどこから取得するのかが変更できる柔軟なプログラムを得る。
    そして今回のリポジトリはメモリ上からデータを取得する。
    重要なことは、リポジトリクラスがユースケース関数内で呼び出す処理を実装している必要があるということだけだ。
    ユースケースにとってはどこからどのようにデータを取得するのかは関係ない。
    MySQL などから取得するわけではないので、アプリケーションが起動していないときはどこにもデータは存在しない。

---
+++ rentomatic/repository/memory_repository.py
@@ -0,0 +1,9 @@
+from ..domain.room import Room
+
+
+class MemoryRepository:
+    def __init__(self, data):
+        self.data = data
+
+    def list(self):
+        return [Room.from_dict(i) for i in self.data]
